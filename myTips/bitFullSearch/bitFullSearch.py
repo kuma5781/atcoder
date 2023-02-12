f40665649fcd8a5f16a548455f8ecf0ab7fe3989
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-02-12 16:36:47 +0900

    edit bitFullSearch

--- myTips/bitFullSearch/bitFullSearch.py
+++ myTips/bitFullSearch/bitFullSearch.py
@@ -3,6 +3,8 @@ def bitFullSearch(list):
     for i in range(1 << len(list)):
         combination = []
         for j in range(len(list)):
+            # 要素確認用print
+            print(bin(i >> j))
             if ((i >> j) & 1):
                 combination.append(list[j])
         print(combination)
