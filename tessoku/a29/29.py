10b18f3e86ea50fa169f4a3e373c8fa6d52c51d1
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-10-29 19:46:50 +0900

    a29 繰り返し二乗法　別解追加

--- tessoku/a29/29.py
+++ tessoku/a29/29.py
@@ -12,3 +12,37 @@ N = 1000000007
 a, b = map(int, input().split())
 print(Power(a, b, N))
 
+"""
+繰り返し二乗法
+
+Parameters
+----------
+x : int
+    底
+n : int
+    指数
+m : int
+    剰余
+"""
+def myPow(x, n, m):
+    ans = 1
+    # すべての桁を1かどうか確認して、1であれば答えにxをかける
+    while n > 0:
+        # 最小桁が1のときだけ、ansにxをかける
+        if n % 2 == 1:
+            ans = ans * x % m
+        # 次の桁数が1の場合xの2乗を答えに掛ける必要があるので、ここでxを2乗しておく
+        x = x * x % m
+        # 次の桁へ
+        n >>= 1
+    return ans
+
+N = 1000000007
+a, b = map(int, input().split())
+print(myPow(a, b, N))
+
+# pythonのpow関数を使う
+N = 1000000007
+a, b = map(int, input().split())
+print(pow(a, b, N))
+
