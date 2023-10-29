6a4da1330f037bc46d3634156ff30835ee67fcb4
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-10-29 10:03:18 +0900

    a11 コメント追加、変数名修正

--- tessoku/a11/a11.py
+++ tessoku/a11/a11.py
@@ -1,19 +1,32 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
+"""
+二分探索
 
-def search(X, A, N):
+Parameters
+----------
+X : int
+    探したい値
+list : list
+    探索対象の配列
+"""
+def list_binary_search(x, list):
+    N = len(list)
+    # 二分探索では必ずソートされていることが前提
+    sorted_list = sorted(list)
     L = 0
     R = N - 1
+    # L=Rになったら最後の探索対象になるので、L<=Rとする
     while L <= R:
         M = (L + R) // 2
-        if X < A[M]:
+        if x < sorted_list[M]:
             R = M - 1
-        if X == A[M]:
+        if x == sorted_list[M]:
             return M
-        if X > A[M]:
+        if x > sorted_list[M]:
             L = M + 1
     return -1
 
-ans = search(X, A, N)
+ans = list_binary_search(X, A)
 print(ans + 1)
