8b94fda4ee31bf86a81dc19704799cf16ce124ca
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-10-30 23:12:27 +0900

    abc326 A

---
+++ practice/abc326/A.py
@@ -0,0 +1,13 @@
+X, Y = map(int, input().split())
+
+if X > Y:
+    if X - Y <= 3:
+        print("Yes")
+    else:
+        print("No")
+else:
+    if Y - X <= 2:
+        print("Yes")
+    else:
+        print("No")
+
